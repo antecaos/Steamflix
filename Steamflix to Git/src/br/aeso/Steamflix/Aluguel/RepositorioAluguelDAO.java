1d2b32b800c9bf062dbfc9432d2f506b7be7b276
antecaos <giuliocaetano@yahoo.com.br>
2015-10-26 18:45:26 -0300

    Implementando a classe RepositorioAluguelDAO

--- Steamflix to Git/src/br/aeso/Steamflix/Aluguel/RepositorioAluguelDAO.java
+++ Steamflix to Git/src/br/aeso/Steamflix/Aluguel/RepositorioAluguelDAO.java
@@ -1,5 +1,50 @@
 package br.aeso.Steamflix.Aluguel;
 
-public class RepositorioAluguelDAO {
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import br.aeso.Steamflix.JDBC.ConnectionFactory;
+
+public class RepositorioAluguelDAO implements IRepositorioAluguel{
+	private Connection connection;
+
+	public RepositorioAluguelDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+	@Override
+	public void cadastrar(Aluguel aluguel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Aluguel aluguel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remover(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Aluguel procurar(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existe(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Aluguel> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
