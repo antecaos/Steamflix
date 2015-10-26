30a22e59e3cd9601ea2b5dad9ba461c201aa23d2
antecaos <giuliocaetano@yahoo.com.br>
2015-10-26 20:03:39 -0300

    Pequenas modificaçes

--- Steamflix to Git/src/br/aeso/Steamflix/Aluguel/RepositorioAluguelDAO.java
+++ Steamflix to Git/src/br/aeso/Steamflix/Aluguel/RepositorioAluguelDAO.java
@@ -8,23 +8,28 @@ import java.util.ArrayList;
 
 import br.aeso.Steamflix.JDBC.ConnectionFactory;
 
-public class RepositorioAluguelDAO implements IRepositorioAluguel{
+public class RepositorioAluguelDAO implements IRepositorioAluguel {
 	private Connection connection;
 
 	public RepositorioAluguelDAO() {
 		this.connection = new ConnectionFactory().getConnection();
 	}
+
 	@Override
 	public void cadastrar(Aluguel aluguel) {
 		// TODO Auto-generated method stub
-		String sql = "insert into Steamflix.Aluguel()values(??)";
+		String sql = "insert into Steamflix.Aluguel(dataAluguel,dataDevolucaoAluguel,"
+				+ "idClienteAluguel,precoAluguel,idCupomAluguel)values(?,?,?,?,?)";
 		try {
 			// prepared statement para a inserção
 			PreparedStatement stmt = connection.prepareStatement(sql);
 
 			// seta os valores
-			stmt.setInt(1, aluguel.getId());
-						
+			stmt.setDate(1, new Date(aluguel.getData().getTimeInMillis()));
+			stmt.setDate(2, new Date(aluguel.getDataDevolucao().getTimeInMillis()));
+			stmt.setString(3, aluguel.getCliente().getCPF());
+			stmt.setDouble(4, aluguel.getPreco());
+			stmt.setInt(5, aluguel.getCupom().getId());
 
 			// executa
 			stmt.execute();
@@ -37,13 +42,13 @@ public class RepositorioAluguelDAO implements IRepositorioAluguel{
 	@Override
 	public void atualizar(Aluguel aluguel) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void remover(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
