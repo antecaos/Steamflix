98815ca8ca69bb96934eb13f14dc13aef419c57e
antecaos <giuliocaetano@yahoo.com.br>
2015-11-11 19:20:36 -0300

    Commit maneiro

--- Steamflix to Git/src/br/aeso/Steamflix/main/TelaCliente.java
+++ Steamflix to Git/src/br/aeso/Steamflix/main/TelaCliente.java
@@ -19,11 +19,19 @@ import com.jgoodies.forms.factories.FormFactory;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import javax.swing.JComboBox;
 
 public class TelaCliente extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -62,13 +70,29 @@ public class TelaCliente extends JFrame {
 		scrollPane.setViewportView(tabbedPane);
 		
 		JPanel panel = new JPanel();
-		tabbedPane.addTab("New tab", null, panel, null);
+		tabbedPane.addTab("Dados", null, panel, null);
 		panel.setLayout(new FormLayout(new ColumnSpec[] {
 				FormFactory.RELATED_GAP_COLSPEC,
 				FormFactory.DEFAULT_COLSPEC,
 				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
+				ColumnSpec.decode("max(23dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(34dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(36dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(34dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(125dlu;default)"),},
 			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
@@ -82,16 +106,73 @@ public class TelaCliente extends JFrame {
 		panel.add(lblNewLabel, "2, 4, right, default");
 		
 		textField = new JTextField();
-		panel.add(textField, "4, 4, fill, default");
+		panel.add(textField, "4, 4, 7, 1, fill, default");
 		textField.setColumns(10);
 		
+		JLabel lblCpf = new JLabel("CPF");
+		panel.add(lblCpf, "2, 6, right, default");
+		
+		textField_3 = new JTextField();
+		panel.add(textField_3, "4, 6, fill, default");
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel.add(lblNewLabel_2, "6, 6, right, default");
+		
+		textField_4 = new JTextField();
+		panel.add(textField_4, "8, 6, left, default");
+		textField_4.setColumns(10);
+		
 		JLabel lblEndereo = new JLabel("Endereço");
-		panel.add(lblEndereo, "2, 8");
+		panel.add(lblEndereo, "2, 8, right, default");
+		
+		JLabel label = new JLabel("New label");
+		panel.add(label, "2, 10, right, default");
+		
+		textField_1 = new JTextField();
+		panel.add(textField_1, "4, 10, 7, 1");
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel.add(lblNewLabel_1, "12, 10, right, default");
+		
+		textField_2 = new JTextField();
+		panel.add(textField_2, "16, 10, left, default");
+		textField_2.setColumns(5);
+		
+		JLabel lblBairro = new JLabel("Complemento");
+		panel.add(lblBairro, "2, 12, right, default");
+		
+		textField_5 = new JTextField();
+		panel.add(textField_5, "4, 12, fill, default");
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		panel.add(lblNewLabel_3, "6, 12, right, default");
+		
+		textField_7 = new JTextField();
+		panel.add(textField_7, "8, 12, fill, default");
+		textField_7.setColumns(10);
+		
+		JLabel label_1 = new JLabel("New label");
+		panel.add(label_1, "10, 12, right, default");
+		
+		textField_6 = new JTextField();
+		panel.add(textField_6, "12, 12, fill, default");
+		textField_6.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		panel.add(lblNewLabel_4, "14, 12, right, default");
+		
+		JComboBox comboBox = new JComboBox();
+		panel.add(comboBox, "16, 12, left, default");
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_1, null);
+		panel_1.setLayout(null);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(null);
 	}
 }
