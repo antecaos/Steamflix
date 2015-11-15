f506a9217b090b705da0777674b687e08a4f9e55
antecaos <giuliocaetano@yahoo.com.br>
2015-11-15 00:28:55 -0300

    Campos nulos Cadastro

--- Steamflix to Git/src/br/aeso/Steamflix/Cadastro/CamposNulosCadastro.java
+++ Steamflix to Git/src/br/aeso/Steamflix/Cadastro/CamposNulosCadastro.java
@@ -4,7 +4,8 @@ public class CamposNulosCadastro {
 	public boolean estaVazio(Cadastro cadastro) {
 		Boolean flag = false;
 		if (cadastro.getLogin().trim().isEmpty()
-				|| cadastro.getSenha().trim().isEmpty()) {
+				|| cadastro.getSenha().trim().isEmpty()
+				|| cadastro.getEmailPrincipal().trim().isEmpty()) {
 			flag = true;
 		}
 		return flag;
